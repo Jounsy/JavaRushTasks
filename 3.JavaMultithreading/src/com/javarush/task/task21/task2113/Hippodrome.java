50bc7909b8d7ec0d3dcdcd0fc6ae1de3117a25a1
Jouns <Jouns@mail.ru>
2017-05-18 09:37:36 +0400

    Start

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task21.task2113;
+
+/**
+ * Created by Work-TESTER on 18.05.2017.
+ */
+public class Hippodrome {
+    public static void main(String[] args) {
+
+    }
+}
