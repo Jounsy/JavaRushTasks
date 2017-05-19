ce46bd0e9e53dfaa1c872b7633e973a0c3656feb
Jouns <Jouns@mail.ru>
2017-05-19 08:10:52 +0400

    Add constructor and initialization List horses

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,17 +1,22 @@
 package com.javarush.task.task21.task2113;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 /**
  * Created by Work-TESTER on 18.05.2017.
  */
 public class Hippodrome {
+
+    public Hippodrome(List horses)
+    {
+        this.horses = horses;
+    }
     public List<Horse> getHorses() {
         return horses;
     }
 
-    private List<Horse> horses = new ArrayList<>();
+    private List<Horse> horses;
     public static void main(String[] args) {
 
     }
