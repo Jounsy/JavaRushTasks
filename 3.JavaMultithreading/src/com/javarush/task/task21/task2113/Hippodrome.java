d7394d3050a9104c1fbe1707aa1a9bdd2c15a1e1
Jouns <Jouns@mail.ru>
2017-05-19 09:26:41 +0400

    Add static field game, init List horses + 3 horses into

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task21.task2113;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,6 +9,7 @@ import java.util.List;
  */
 public class Hippodrome {
 
+    static Hippodrome game;
     public Hippodrome(List horses)
     {
         this.horses = horses;
@@ -17,7 +19,16 @@ public class Hippodrome {
     }
 
     private List<Horse> horses;
+
     public static void main(String[] args) {
+        game = new Hippodrome(new ArrayList());
+
+        game.getHorses().add(new Horse("Tattoo",3.0,0.0));
+        game.getHorses().add(new Horse("Eclipse",3.0,0.0));
+        game.getHorses().add(new Horse("Idea",3.0,0.0));
+
+
+
 
     }
 }
