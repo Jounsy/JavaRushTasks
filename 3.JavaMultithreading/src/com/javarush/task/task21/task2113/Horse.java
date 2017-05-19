26d5dc0189ab191d18e6a3a6f2073a78391a6328
Jouns <Jouns@mail.ru>
2017-05-19 08:20:45 +0400

    Created private fields in Horse and also Getters and Setters

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Horse.java
@@ -4,4 +4,31 @@ package com.javarush.task.task21.task2113;
  * Created by Work-TESTER on 18.05.2017.
  */
 public class Horse {
+    private String name;
+    private double speed;
+    private double distance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public Double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Double distance) {
+        this.distance = distance;
+    }
 }
