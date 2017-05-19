e937e300dba01659002db62ad79e27ef091909d5
Jouns <Jouns@mail.ru>
2017-05-19 08:04:22 +0400

    Add private List Horses and getter for him

--- 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2113/Hippodrome.java
@@ -1,9 +1,17 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Work-TESTER on 18.05.2017.
  */
 public class Hippodrome {
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    private List<Horse> horses = new ArrayList<>();
     public static void main(String[] args) {
 
     }
