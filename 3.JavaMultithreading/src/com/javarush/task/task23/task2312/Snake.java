56da65fc847653b55e50fcffc473a37ab5681fd4
Jouns <Jouns@mail.ru>
2017-06-06 10:31:24 +0400

    Big task 2312, Snake game, add fields for Snake

--- 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
+++ 3.JavaMultithreading/src/com/javarush/task/task23/task2312/Snake.java
@@ -1,7 +1,30 @@
 package com.javarush.task.task23.task2312;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Work-TESTER on 06.06.2017.
  */
 public class Snake {
+    private List<SnakeSection> sections = new ArrayList<>();
+    private boolean isAlive;
+
+    public List<SnakeSection> getSections() {
+        return sections;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public SnakeDirection getDirection() {
+        return direction;
+    }
+
+    private SnakeDirection direction;
+
+    public void setDirection(SnakeDirection direction) {
+        this.direction = direction;
+    }
 }
