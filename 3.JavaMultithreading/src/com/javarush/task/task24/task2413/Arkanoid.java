c7e7dc18fd9bab63431568fe81cf56c49c525124
Jouns <Jouns@mail.ru>
2017-06-14 09:03:21 +0400

    BigTask24.Arkanoid / composition

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -1,11 +1,18 @@
 package com.javarush.task.task24.task2413;
 
+import java.util.List;
+
 /**
  * Created by Work-TESTER on 14.06.2017.
  */
 public class Arkanoid {
     private int width;
     private int height;
+    private Ball ball;
+    private Stand stand;
+    private List<Brick> bricks;
+
+
 
     public Arkanoid(int width, int height){
         this.width = width;
@@ -30,5 +37,28 @@ public class Arkanoid {
 
     public void setHeight(int height) {
         this.height = height;
+    }
+    public Ball getBall() {
+        return ball;
+    }
+
+    public void setBall(Ball ball) {
+        this.ball = ball;
+    }
+
+    public Stand getStand() {
+        return stand;
+    }
+
+    public void setStand(Stand stand) {
+        this.stand = stand;
+    }
+
+    public List<Brick> getBricks() {
+        return bricks;
+    }
+
+    public void setBricks(List<Brick> bricks) {
+        this.bricks = bricks;
     }
 }
