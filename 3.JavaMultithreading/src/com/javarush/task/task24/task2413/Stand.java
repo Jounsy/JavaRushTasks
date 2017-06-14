d8a243e3d47d0012be455c5d2fcc209e0fc877f9
Jouns <Jouns@mail.ru>
2017-06-14 14:35:32 +0400

    BigTask24.Arkanoid / Stand

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -4,17 +4,29 @@ package com.javarush.task.task24.task2413;
  * Created by Work-TESTER on 14.06.2017.
  */
 public class Stand extends BaseObject {
-    public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+
+    private double speed;
+    private double direction;
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
     }
 
     @Override
     void draw(Canvas canvas) {
-
     }
 
     @Override
     void move() {
+    }
 
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
     }
 }
