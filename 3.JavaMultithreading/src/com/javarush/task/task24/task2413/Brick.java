c0f5d6e21688110cfb86680400b4577d96ad555d
Jouns <Jouns@mail.ru>
2017-06-14 10:53:33 +0400

    BigTask24.Arkanoid / Brick

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -4,8 +4,9 @@ package com.javarush.task.task24.task2413;
  * Created by Work-TESTER on 14.06.2017.
  */
 public class Brick extends BaseObject {
-    public Brick(double x, double y, double radius) {
-        super(x, y, radius);
+
+    public Brick(double x, double y) {
+        super(x, y, 3);
     }
 
     @Override
