7a6e88806d1e554b029d03965a315cf25d8202de
Jouns <Jouns@mail.ru>
2017-06-14 09:41:39 +0400

    BigTask24.Arkanoid / create fields in BaseObject and constructor in BaseObject & extends classes

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -4,4 +4,7 @@ package com.javarush.task.task24.task2413;
  * Created by Work-TESTER on 14.06.2017.
  */
 public class Ball extends BaseObject {
+    public Ball(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Brick.java
@@ -4,4 +4,7 @@ package com.javarush.task.task24.task2413;
  * Created by Work-TESTER on 14.06.2017.
  */
 public class Brick extends BaseObject {
+    public Brick(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
