9251026a1039e188f29e40bac46c025244a7f0bb
Jouns <Jouns@mail.ru>
2017-06-14 16:24:18 +0400

    BigTask24.Arkanoid / Stand

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -20,6 +20,17 @@ public class Stand extends BaseObject {
 
     @Override
     void move() {
+        x += speed * direction;
+    }
+
+    public void draw(){}
+
+    public void moveLeft(){
+        direction = -1;
+    }
+
+    public void moveRight(){
+        direction = 1;
     }
 
     public double getSpeed() {
