4908fff4464eec3fcbb72c0e5da89481e3243486
Jouns <Jouns@mail.ru>
2017-06-14 08:31:43 +0400

    BigTask24.Arkanoid / create fields width height

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Arkanoid.java
@@ -4,7 +4,26 @@ package com.javarush.task.task24.task2413;
  * Created by Work-TESTER on 14.06.2017.
  */
 public class Arkanoid {
+    private int width;
+    private int height;
+
     public static void main(String[] args) {
 
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
