624ac0c44b1345b4e37c643c192cda5fe6d2bb76
Jouns <Jouns@mail.ru>
2017-06-14 09:33:29 +0400

    BigTask24.Arkanoid / create classes BaseObject, Canvas

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
@@ -3,5 +3,5 @@ package com.javarush.task.task24.task2413;
 /**
  * Created by Work-TESTER on 14.06.2017.
  */
-public class Ball extends BaseObject {
+public class BaseObject {
 }
--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -3,5 +3,5 @@ package com.javarush.task.task24.task2413;
 /**
  * Created by Work-TESTER on 14.06.2017.
  */
-public class Stand {
+public class Stand extends BaseObject {
 }
