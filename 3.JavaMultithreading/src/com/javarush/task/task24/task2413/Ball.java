0f8b92b3f3d09adf43b6484cec24464719da3e23
Jouns <Jouns@mail.ru>
2017-06-14 11:13:58 +0400

    BigTask24.Arkanoid / Ball

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -4,8 +4,17 @@ package com.javarush.task.task24.task2413;
  * Created by Work-TESTER on 14.06.2017.
  */
 public class Ball extends BaseObject {
-    public Ball(double x, double y, double radius) {
-        super(x, y, radius);
+
+    private double speed;
+    private double direction;
+    private double dx = speed * direction;
+    private double dy = speed * direction;
+    private boolean isFrozen = true;
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
     }
 
     @Override
@@ -16,5 +25,37 @@ public class Ball extends BaseObject {
     @Override
     void move() {
 
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public void setDy(double dy) {
+        this.dy = dy;
     }
 }
