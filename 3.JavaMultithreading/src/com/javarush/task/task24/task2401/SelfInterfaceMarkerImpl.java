959129d0e0364fd595ed843e558d354a5089b1de
Jouns <Jouns@mail.ru>
2017-06-15 16:41:06 +0400

    BigTask24.Arkanoid / task2401

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by Work-TESTER on 15.06.2017.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+
+    public void mover(){}
+
+    public void fly(){}
+}
