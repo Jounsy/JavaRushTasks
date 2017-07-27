48fe6ed582f6bb863d10fce2557120ffd47d323e
Jouns <Jouns@mail.ru>
2017-07-27 11:17:33 +0400

    BigTask30_CHAT/3

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
@@ -4,4 +4,5 @@ package com.javarush.task.task30.task3008;
  * Created by Work-TESTER on 26.07.2017.
  */
 public enum MessageType {
+    NAME_REQUEST, USER_NAME, NAME_ACCEPTED, TEXT, USER_ADDED, USER_REMOVED
 }
