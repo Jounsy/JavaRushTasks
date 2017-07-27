405c73a3e076f5db1b271964bff89fb3db92d5f4
Jouns <Jouns@mail.ru>
2017-07-27 11:28:34 +0400

    BigTask30_CHAT/4

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Message.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Message.java
@@ -1,7 +1,30 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.Serializable;
+
 /**
  * Created by Work-TESTER on 26.07.2017.
  */
-public class Message {
+public class Message implements Serializable {
+
+   private final MessageType type;
+   private final String data;
+
+    public Message(MessageType type) {
+        this.type = type;
+        this.data = null;
+    }
+
+    public Message(MessageType type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public MessageType getType() {
+        return type;
+    }
+
+    public String getData() {
+        return data;
+    }
 }
