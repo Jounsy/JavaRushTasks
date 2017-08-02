7eabbe2d147a21a2be9b135a51e09d2ca4cf557c
Jouns <Jouns@mail.ru>
2017-08-02 09:45:31 +0400

    BigTask30_CHAT/12 (add pkg client & class Client)

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task30.task3008.client;
+
+import com.javarush.task.task30.task3008.Connection;
+
+/**
+ * Created by Work-TESTER on 02.08.2017.
+ */
+public class Client {
+
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+   public class SocketThread extends Thread{
+
+    }
+}
