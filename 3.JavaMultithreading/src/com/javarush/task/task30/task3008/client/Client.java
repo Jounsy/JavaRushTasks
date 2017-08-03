0e5310fb9893ce9e7fecd3053dd0eba9d09a97c4
Jouns <Jouns@mail.ru>
2017-08-03 14:34:14 +0400

    BigTask30_CHAT/14

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -4,25 +4,57 @@ import com.javarush.task.task30.task3008.Connection;
 import com.javarush.task.task30.task3008.ConsoleHelper;
 import com.javarush.task.task30.task3008.Message;
 import com.javarush.task.task30.task3008.MessageType;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+
 
 /**
  * Created by Work-TESTER on 02.08.2017.
  */
-public class Client {
+public class Client{
 
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+
+    public void run(){
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+        synchronized (this) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                ConsoleHelper.writeMessage("Error Waiting");
+                try {
+                    connection.close();
+                } catch (IOException e1) {}
+            }
+        }
+
+        if (clientConnected) {
+            ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+        }
+        else ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+        while (clientConnected) {
+            String message;
+            if (!(message = ConsoleHelper.readString()).equalsIgnoreCase("exit")) {
+                if (shouldSendTextFromConsole())
+                    sendTextMessage(message);
+            }
+            else break;
+        }
+
+    }
 
    public class SocketThread extends Thread{
 
     }
 
     protected String getServerAddress(){
-
         ConsoleHelper.writeMessage("Введи IP адрес");
         return ConsoleHelper.readString();
     }
@@ -31,6 +63,7 @@ public class Client {
         ConsoleHelper.writeMessage("Введи номер порта");
         return ConsoleHelper.readInt();
     }
+
     protected String getUserName(){
         ConsoleHelper.writeMessage("Введи имя");
         return ConsoleHelper.readString();
