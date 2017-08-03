6c1e289c345431c0ca8b3042e9a62651780acde3
Jouns <Jouns@mail.ru>
2017-08-03 12:03:34 +0400

    BigTask30_CHAT/12 (add pkg client & class Client)

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,6 +1,13 @@
 package com.javarush.task.task30.task3008.client;
 
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by Work-TESTER on 02.08.2017.
@@ -13,4 +20,38 @@ public class Client {
    public class SocketThread extends Thread{
 
     }
+
+    protected String getServerAddress(){
+
+        ConsoleHelper.writeMessage("Введи IP адрес");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort(){
+        ConsoleHelper.writeMessage("Введи номер порта");
+        return ConsoleHelper.readInt();
+    }
+    protected String getUserName(){
+        ConsoleHelper.writeMessage("Введи имя");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT,text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Error");
+            clientConnected = false;
+        }
+    }
+
+
 }
