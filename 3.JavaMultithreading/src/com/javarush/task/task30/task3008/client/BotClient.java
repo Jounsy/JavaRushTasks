ebe1974fa3340a06c91fd9c142de5652ed8f6773
Jouns <Jouns@mail.ru>
2017-08-08 11:14:12 +0400

    BigTask30_CHAT/19

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/BotClient.java
@@ -1,12 +1,62 @@
 package com.javarush.task.task30.task3008.client;
 
+import com.javarush.task.task30.task3008.ConsoleHelper;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * Created by Work-TESTER on 07.08.2017.
  */
 public class BotClient extends Client {
 
     public class BotSocketThread extends SocketThread{
+        @Override
+        protected void processIncomingMessage(String message) {
 
+            ConsoleHelper.writeMessage(message);
+            String userName;
+            String text;
+
+            if (message.contains(": ")) {
+                String textMessage[] = message.split(": ", 2);
+                userName = textMessage[0];
+                text = textMessage[1];
+
+
+                SimpleDateFormat formatDate = null;
+
+                if (text.equals("дата")) {
+                    formatDate = new SimpleDateFormat("d.MM.YYYY");
+                } else if (text.equals("день")) {
+                    formatDate = new SimpleDateFormat("d");
+                } else if (text.equals("месяц")) {
+                    formatDate = new SimpleDateFormat("MMMM");
+                } else if (text.equals("год")) {
+                    formatDate = new SimpleDateFormat("YYYY");
+                } else if (text.equals("время")) {
+                    formatDate = new SimpleDateFormat("H:mm:ss");
+                } else if (text.equals("час")) {
+                    formatDate = new SimpleDateFormat("H");
+                } else if (text.equals("минуты")) {
+                    formatDate = new SimpleDateFormat("m");
+                } else if (text.equals("секунды")) {
+                    formatDate = new SimpleDateFormat("s");
+                }
+                if (!(formatDate == null)) {
+                    sendTextMessage("Информация для " + userName + ": " + formatDate.format(Calendar.getInstance().getTime()));
+                }
+
+            }
+        }
+
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+
+        }
     }
 
     @Override
@@ -30,4 +80,6 @@ public class BotClient extends Client {
         BotClient botClient = new BotClient();
         botClient.run();
     }
+
+
 }
