7f9dc732b9caffb2a6b75ad0af27dc108c5926c5
Jouns <Jouns@mail.ru>
2017-08-16 15:48:19 +0400

    BIG_TASK_Archivator\task1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,40 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by Work-TESTER on 16.08.2017.
+ */
+public class Archiver {
+    public static void main(String[] args) {
+        System.out.println("Enter full path to archive");
+        BufferedReader readerArchive = new BufferedReader(new InputStreamReader(System.in));
+        String fullArchivePath = null;
+        try {
+            fullArchivePath = readerArchive.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Path absoluteArchivePath = Paths.get(fullArchivePath);
+        ZipFileManager zipFileManager = new ZipFileManager(absoluteArchivePath);
+
+        System.out.println("Enter full path to file");
+        BufferedReader readerFile = new BufferedReader(new InputStreamReader(System.in));
+        String fullFilePath = null;
+        try {
+            fullArchivePath = readerFile.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Path absoluteFilePath = Paths.get(fullArchivePath);
+        try {
+            zipFileManager.createZip(absoluteFilePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task31.task3110;
+
+import java.nio.file.Path;
+
+/**
+ * Created by Work-TESTER on 16.08.2017.
+ */
+public class ZipFileManager {
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile){
+        this.zipFile = zipFile;
+    }
+
+    public void createZip(Path source) throws Exception{
+
+    }
+
+
+}
