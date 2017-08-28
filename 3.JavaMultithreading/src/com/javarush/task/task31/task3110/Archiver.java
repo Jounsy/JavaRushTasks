f7377961f7d23dd5596eb734fd9ac005fb736dd0
Jouns <Jouns@mail.ru>
2017-08-28 10:30:26 +0400

    BIG_TASK_Archivator\task8 (change main)

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,38 +14,46 @@ import java.nio.file.Paths;
  */
 public class Archiver {
     public static void main(String[] args) {
-        System.out.println("Enter full path to archive");
-        BufferedReader readerArchive = new BufferedReader(new InputStreamReader(System.in));
-        String fullArchivePath = null;
-        try {
-            fullArchivePath = readerArchive.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Path absoluteArchivePath = Paths.get(fullArchivePath);
-        ZipFileManager zipFileManager = new ZipFileManager(absoluteArchivePath);
 
-        System.out.println("Enter full path to file");
-        BufferedReader readerFile = new BufferedReader(new InputStreamReader(System.in));
-        String fullFilePath = null;
-        try {
-            fullArchivePath = readerFile.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Path absoluteFilePath = Paths.get(fullArchivePath);
-        try {
-            zipFileManager.createZip(absoluteFilePath);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-        ExitCommand exitCommand = new ExitCommand();
-        try {
-            exitCommand.execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
 
+        } while (operation != Operation.EXIT);
+
+
+    }
+
+    public static Operation askOperation() throws IOException{
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(Operation.CREATE.ordinal() + " - упаковать файлы в архив");
+        ConsoleHelper.writeMessage(Operation.ADD.ordinal() + " - добавить файл в архив");
+        ConsoleHelper.writeMessage(Operation.REMOVE.ordinal() + " - удалить файл из архива");
+        ConsoleHelper.writeMessage(Operation.EXTRACT.ordinal() + " - распаковать архив");
+        ConsoleHelper.writeMessage(Operation.CONTENT.ordinal() + " - просмотреть содержимое архива");
+        ConsoleHelper.writeMessage(Operation.EXIT.ordinal() + " - выход");
+
+        int operationNumber =  ConsoleHelper.readInt();
+        if (operationNumber==Operation.CREATE.ordinal())
+            return Operation.CREATE;
+        if (operationNumber==Operation.ADD.ordinal())
+            return Operation.ADD;
+        if (operationNumber==Operation.REMOVE.ordinal())
+            return Operation.REMOVE;
+        if (operationNumber==Operation.EXTRACT.ordinal())
+            return Operation.EXTRACT;
+        if (operationNumber==Operation.CONTENT.ordinal())
+            return Operation.CONTENT;
+        if (operationNumber==Operation.EXIT.ordinal())
+            return Operation.EXIT;
+        return null;
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/exception/PathIsNotFoundException.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/exception/PathIsNotFoundException.java
@@ -1,7 +1,4 @@
 package com.javarush.task.task31.task3110.exception;
 
-/**
- * Created by Work-TESTER on 24.08.2017.
- */
 public class PathIsNotFoundException extends Exception {
 }
